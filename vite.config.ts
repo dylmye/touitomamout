c53385ae69083322fb6ff13930921e21a1348433
Dylan Myers <git-signed@dylmye.me>
2025-10-11 23:00:38 +0100

    fix: don't bundle cycletls dependency

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,20 @@ import dts from "vite-plugin-dts";
 import pkg from "./package.json";
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: "./src/index.ts",
-      formats: ["es"], // pure ESM package
-      fileName: "index",
-    },
-    rollupOptions: {
-      external: [
-        ...Object.keys(pkg.dependencies), // don't bundle dependencies
-        /^node:.*/, // don't bundle built-in Node.js modules (use protocol imports!)
-      ],
-    },
-    target: "esnext", // transpile as little as possible
-  },
-  plugins: [dts()],
+	build: {
+		lib: {
+			entry: "./src/index.ts",
+			formats: ["es"], // pure ESM package
+			fileName: "index",
+		},
+		rollupOptions: {
+			external: [
+				...Object.keys(pkg.dependencies), // don't bundle dependencies
+				"@the-convocation/twitter-scraper/cycletls", // addition to above
+				/^node:.*/, // don't bundle built-in Node.js modules (use protocol imports!)
+			],
+		},
+		target: "esnext", // transpile as little as possible
+	},
+	plugins: [dts()],
 });
