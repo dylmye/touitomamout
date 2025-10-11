d47c5e0f3ee63dc1fc39401fd8079af17bb0e8e9
Dylan Myers <git-signed@dylmye.me>
2025-10-11 19:10:57 +0100

    fix(ci): build-info with bun; upgrade bun version

--- scripts/build-info.sh
+++ scripts/build-info.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-VERSION=$(npm pkg get version | xargs)
+VERSION=$(bun pm version | sed -n 's/^Current package version: v\([0-9.]*\)$/\1/p')
 echo "{ \"name\": \"Touitomamout\", \"version\": \"$VERSION\" }" > ./src/buildInfo.json
